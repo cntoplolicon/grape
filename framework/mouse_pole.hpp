4e00028bcc1ffe6c2cb0ccdf1069471beac0c9b5
cntoplolicon <cntoplolicon@gmail.com>
2014-11-07 14:37:35 +0800

    build glutil on osx, fix mouse movmenet

--- framework/mouse_pole.hpp
+++ framework/mouse_pole.hpp
@@ -6,6 +6,10 @@
 #include "glm/glm.hpp"
 #include "glutil/MousePoles.h"
 
+const int MOUSE_BUTTON_LEFT = 1;
+const int MOUSE_BUTTON_MIDDLE = 2;
+const int MOUSE_BUTTON_RIGHT = 4;
+
 template <typename T>
 inline void forwardMouseMotionToMousePole(T &pole, GLUSint x, GLUSint y)
 {
@@ -16,20 +20,14 @@ template<typename T>
 inline void forwardMouseClickToMousePole(T &pole, GLUSboolean pressed, GLUSint button, GLUSint x, GLUSint y)
 {
     glutil::MouseButtons eButton;
-    switch(button)
-    {
-    case GLFW_MOUSE_BUTTON_LEFT:
-        std::cout << "left button" << std::endl;
+    if (button & MOUSE_BUTTON_LEFT) {
         eButton = glutil::MB_LEFT_BTN;
-        break;
-    case GLFW_MOUSE_BUTTON_MIDDLE:
-        std::cout << "middle button" << std::endl;
+    }
+    if (button & MOUSE_BUTTON_MIDDLE) {
         eButton = glutil::MB_MIDDLE_BTN;
-        break;
-    case GLFW_MOUSE_BUTTON_RIGHT:
-        std::cout << "right button" << std::endl;
+    }
+    if (button & MOUSE_BUTTON_RIGHT) {
         eButton = glutil::MB_RIGHT_BTN;
-        break;
     }
     pole.MouseClick(eButton, pressed, 0, glm::ivec2(x, y));
 }
@@ -37,8 +35,11 @@ inline void forwardMouseClickToMousePole(T &pole, GLUSboolean pressed, GLUSint b
 template<typename T>
 inline void forwardMouseWheelToMousePole(T &pole, GLUSint wheel, GLUSint ticks, GLUSint x, GLUSint y)
 {
-    std::cout << "wheel: " << ticks << std::endl;
-    pole.MouseWheel(ticks, 0, glm::ivec2(x, y));
+    static GLUSint lastTicks = 0;
+    if (ticks != lastTicks) {
+        pole.MouseWheel(ticks - lastTicks, 0, glm::ivec2(x, y));
+    }
+    lastTicks = ticks;
 }
 
 #endif
